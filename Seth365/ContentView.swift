5927ff4e69a6f88e36cc1052f93f8a1707e35ab6
dufutao <sam79v9streat@hotmail.com>
2025-12-11 13:10:58 +0800

    Initial Commit

---
+++ Seth365/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Seth365
+//
+//  Created by 刘文骏 on 2025/12/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Seth365/Seth365App.swift
@@ -0,0 +1,17 @@
+//
+//  Seth365App.swift
+//  Seth365
+//
+//  Created by 刘文骏 on 2025/12/11.
+//
+
+import SwiftUI
+
+@main
+struct Seth365App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
